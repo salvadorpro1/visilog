0c2e3861dbde6f8a4e10273d0d435ed108407c61
salvador sanchez <salvadorpro1@gmail.com>
2024-11-12 10:15:31 -0400

    borre los comandos de sedeers que no son necesarios y no estan completos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(FilialSeeder::class);
-        $this->call(GerenciaSeeder::class);
     }
 
     
